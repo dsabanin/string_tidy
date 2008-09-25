6eb5735e8e8a311c41b8c3aee4d5a8d42bed0074
Dima Sabanin <sdmitry@gmail.com>
2008-09-25 17:26:40 +0800

    Setting utf8 as default encoding for tidy input, also set other defaults to minimize changes to string formatting after tidying (:indent => false, :wrap => 0)

--- lib/string_tidy.rb
+++ lib/string_tidy.rb
@@ -1,7 +1,11 @@
 module StringTidy
   
   def tidy(options={})
-    options.reverse_merge!(:output_xhtml => true, :show_body_only => true)
+    options.reverse_merge!(:output_xhtml => true, 
+      :show_body_only => true,
+      :char_encoding => "utf8",
+      :indent => false,
+      :wrap => 0)
     
     Tidy.open(options) do |inst|
       inst.clean(self.gsub(/\302\240/, "\240"))
